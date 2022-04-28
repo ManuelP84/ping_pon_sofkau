e0afe4c98acb2bda92bccd816298f702d551f202
Manuel Pineda <pineda.manuel@gmail.com>
2022-04-28 17:45:17 -0500

    Features ---- Function for speed of the ball, also score increment, and reset method once there is a goal

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ ball.style.marginLeft = '534px'
 let w_Pressed = false
 let s_Pressed = false
 
+let ID
+
+let Vx = -1
+let Vy = -1
+let V = Math.sqrt(Math.pow(Vx, 2) + Math.pow(Vy))
+
 document.addEventListener('keydown', (e) => {       /*Event when a key is pressed*/
     if(e.keyCode == '87'){
         w_Pressed = true
@@ -33,9 +39,39 @@ document.addEventListener('keyup', (e) => {         /*Event when a key is releas
 
 gameLoop()
 
+function reset(){
+    clearInterval(ID)
+    Vx = -1
+    Vy = -1
+    let V = Math.sqrt(Math.pow(Vx, 2) + Math.pow(Vy))
+    ball.style.marginTop = '262px'
+    ball.style.marginLeft = '534px'
+    gameLoop()
+}
+
 function gameLoop(){        /*Flow of the game*/
     setTimeout(() => {
-        setInterval(()=>{
+        ID = setInterval(()=>{
+
+            if(marginLeft(ball) < 0){
+                document.querySelector('#comp-score').innerHTML = Number(document.querySelector('#comp-score').innerHTML) + 1
+                reset()
+                return
+            }
+
+            if((marginLeft(ball) + 20) > 1088){
+                document.querySelector('#user-score').innerHTML = Number(document.querySelector('#user-score').innerHTML) + 1
+                reset()
+                return
+            }
+
+            if(marginTop(ball) < 0 || (marginTop(ball) + 20) > 544){
+                Vy = -Vy
+            }
+
+            ball.style.marginLeft = `${marginLeft(ball) + Vx}px`
+            ball.style.marginTop = `${marginTop(ball) + Vy}px`
+
             if(w_Pressed && marginTop(userPaddle) > 0){
                 userPaddle.style.marginTop = `${marginTop(userPaddle) - 2}px`
             }
